90ea5ac003b4dca6092cd1e8f6b201d835860f1c
Vinsen Muliadi <admin@max-metal.us>
2016-03-29 23:06:50 +0700

    [UPDATE] automatically send to /var/www

--- crawl.py
+++ crawl.py
@@ -1,14 +1,20 @@
-import os, sys, re
+# Vinsen Muliadi - admin@max-metal.us
+# https://github.com/MaX121/LiveStreamingUSD/
+
+# importing basic library
+import os, sys, subprocess
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-link = input('YouTube Link :: ')
-print('Processing Link ...')
 
-# validating URL that inputted by user
-# require internet connection
+# first, the program will mark the pwd as the variable
+# user input the YouTube link to embed on the webpage
+# if the user insert without 'https', automatically add it
+basedir = subprocess.check_output('pwd').decode(encoding='UTF-8')
+link = input('YouTube Link :: ')
 if (link.startswith("youtube.com")) :
     link = "https://" + link
 
+print('Processing Link ...')
 page = urlopen(link)
 
 # get the YouTube video ID
@@ -22,15 +28,15 @@ views = bsoup.find("div", {'class' : 'watch-view-count'}).prettify()
 
 # creating index.html
 # if the file already exist, Python will replace it with the new one
-berkas = open("index.html", "w")
+berkas = open("/var/www/index.html", "w")
 berkas.write("<!doctype html>")
 berkas.write("<html>")
 berkas.write("<head>")
 berkas.write("<title>Proyek Teknologi Informasi - Live Streaming Wisuda</title>")
-berkas.write("<link rel='stylesheet' href='bootstrap_3.3.4/css/bootstrap.min.css' />")
+berkas.write("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' />")
 berkas.write("<style type='text/css'>@import url(https://fonts.googleapis.com/css?family=Open+Sans);</style>")
 berkas.write("<script type='text/javascript' src='http://code.jquery.com/jquery-1.12.2.min.js'></script>")
-berkas.write("<script type='text/javascript' src='script.js'></script>")
+berkas.write("<script type='text/javascript' src='"+basedir+"/script.js'></script>")
 berkas.write("</head>")
 berkas.write("<body>")
 berkas.write("<div class='container-fluid'>")
@@ -42,18 +48,16 @@ berkas.write("</div>")
 berkas.write("</div>")
 berkas.write("<div class='container-fluid' id='menubar-bottom'>")
 berkas.write("<div class='row'>")
-# berkas.write("<div class='col-xs-12'>This site is created by PTI - 2016 | This watched " + views + " times with fee y IDR</div>")
 berkas.write("<div class='col-xs-6'>This site is created by PTI - 2016</div>")
 berkas.write("<div class='col-xs-3'>Jumlah Tayang :: " + views + "</div>")
 berkas.write("<div class='col-xs-3'>Total Pemasukan :: <span id='income'></span> </div>")
 berkas.write("</div>")
 berkas.write("</div>")
 berkas.write("</body>")
-berkas.write("<script src='editable.js'></script>")
+berkas.write("<script src='"+basedir+"/editable.js'></script>")
 berkas.write("</html>")
 berkas.close()
-print('File generate successfully')
 
 # generating done
-print('File ready on index.html at ' + os.path.dirname(os.path.realpath('index.html')))
+print('File generate successfully on /var/www/index.html')
 sys.exit(0)
