17f77775101beda463cc7d530c195d5509948cfe
Vinsen Muliadi <admin@max-metal.us>
2016-04-04 22:58:19 +0700

    re-upload

--- geolocation.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-latitude = input('Latitude :: ')
-longitude = input('Longitude :: ')
-api_key = '';
-page = urlopen('https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&'+api_key+'')
